7c89b8f8c6a177f3960ae74d2ff29e3423060956
Dayyan Hashmi <hashmid@mcmaster.ca>
2022-11-23 16:17:17 -0500

    Serial Communications Barebones has been implemented.

--- DCM/DCM_Functions.py
+++ DCM/DCM_Functions.py
@@ -19,3 +19,34 @@ def get_connection_color(pacemaker_connected):
         connection_colour = "red"
     return connection_colour
 
+def recieve_data():
+
+    FRDM_PORT = 'COM3'
+    SYNC = b'\x16'
+    FN_CODE = b'\x55'
+    MODE = struct.pack("B", 1) #For VOO
+    LRL = struct.pack("B", 1) 
+    AtrialAMP = struct.pack("f", 1000)
+    VentAMP = struct.pack("f", 1000)
+    AtrPW = struct.pack("B", 1)
+    VentPW = struct.pack("B", 1)
+    AtrSens = struct.pack("B", 1)
+    VRP = struct.pack("H", 10) #Unsigned Short Size 2
+    ARP = struct.pack("H", 10)
+
+    Signal_echo = Start + SYNC
+
+    with serial.Serial(FRDM_PORT, 115200) as pacemaker:
+        pacemaker.write(Signal_echo)
+        data = pacemaker.read(18)
+        M = data[0]
+        L = data[1]
+        AA = struct.unpack("f", data[2:5])
+        VA = struct.unpack("f", data[6:9])
+
+    print("From the board")
+    print(M)
+    print(L)
+    print(AA)
+    print(VA)
+
